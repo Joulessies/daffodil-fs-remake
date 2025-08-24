d9ed1b832aa88640e09cec8dff12fb7e5f3a7988
Joulessies <julius.sanjose08@gmail.com>
2025-08-24 12:37:21 +0800

    Test

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -5,6 +5,10 @@ export default function HomePage() {
   return (
     <div>
       <h1 className="home-title">Welcome to Daffodil</h1>
+      <p className="home-description">
+        Daffodil is a platform for creating and sharing your ideas.
+      </p>
+      <button className="home-button">Get Started</button>
     </div>
   );
 }
